7469d45d5655a8e5eb35511a5c609830ba8044a2
taburu1 <taburu1@gmail.com>
2022-12-24 16:19:47 +0900

    Spring Security 적용, 테스트코드 임의사용자설정

--- src/main/java/com/sample/springboot/Application.java
+++ src/main/java/com/sample/springboot/Application.java
@@ -2,7 +2,6 @@ package com.sample.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
 * @package : com.sample.springboot
@@ -12,7 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 * @version: 1.0.0
 * @modified : 
 **/
-@EnableJpaAuditing
+// @EnableJpaAuditing -> config 로 이동
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
